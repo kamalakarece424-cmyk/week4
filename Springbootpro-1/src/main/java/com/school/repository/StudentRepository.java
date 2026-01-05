e04fd978017de82fea901b52aa9c2a45a9cd8058
kamalakarece424-cmyk <kamalakarece424@gmail.com>
2026-01-05 13:33:21 +0530

    Delete Springbootpro-1 directory

--- Springbootpro-1/src/main/java/com/school/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.school.repository;
-
-import com.school.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    // No need to declare findAll(); JpaRepository provides it
-}
--- Springbootpro-1/src/main/java/com/school/service/ResourceNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.school.service;
-
-public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) { super(message); }
-}
-
